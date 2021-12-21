af0747ea9348caaafa0dcd366bc87f7374947fa9
dwhenson <dave.henson@gmail.com>
2021-12-21 16:45:39 +0300

    ✅ ADD: 🐛 FIX: items count

--- src/js/app/items-count.js
+++ src/js/app/items-count.js
@@ -4,6 +4,9 @@ import { todos, itemsLeft } from "./../helpers/elements";
  * Updates the number of items not completed in the todo array
  */
 export function itemsCount() {
-  const count = todos.filter((todo) => todo.completed === false).length;
+  const itemsToCount = JSON.parse(!!localStorage.getItem("savedTodos"))
+    ? JSON.parse(localStorage.getItem("savedTodos"))
+    : todos;
+  const count = itemsToCount.filter((todo) => todo.completed === false).length;
   itemsLeft.textContent = count;
 }
