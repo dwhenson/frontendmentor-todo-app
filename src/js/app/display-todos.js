e157fbfd2cd2507dbf6f92a2ca0d936605dcce3b
dwhenson <dave.henson@gmail.com>
2021-12-22 22:07:02 +0300

    ✅ ADD: basic styles

--- src/js/app/display-todos.js
+++ src/js/app/display-todos.js
@@ -53,7 +53,8 @@ export function displayTodos(todoListItems, filter) {
     const removeButton = document.createElement("button");
     removeButton.dataset.function = "remove";
     removeButton.dataset.id = todo.id;
-    removeButton.textContent = "Remove";
+    removeButton.setAttribute("aria-label", "Remove toto");
+    removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
     todoLi.append(removeButton);
     todoList.append(todoLi);
   }
